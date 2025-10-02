6d98286824c907f163c4f04871fe4e4e3dcfa50c
WebPuma <jonathan@webpuma.com>
2025-10-02 14:12:21 -0600

    Test 2

---
+++ src/unAuth/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="landing-container">
+      <h1>Hello, Landing Page! Test 2</h1>
+    </div>
+  );
+}
+
+export default LandingPage;
